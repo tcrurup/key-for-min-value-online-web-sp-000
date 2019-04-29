d10492c0e48201cf73e94ac045ed9641f031c38a
Anthony Rurup <tcrurup@gmail.com>
2019-04-29 02:51:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   count = 0
-  
+  lowest_value_key = ""
   name_hash.each do |key, value|
-    if count == 0 || value < name_hash[lowest_key_value]
+    if count == 0 
+      lowest_value_key = key    
+    elsif value < name_hash[lowest_key_value]
       lowest_key_value = key  
     end
     count += 1
